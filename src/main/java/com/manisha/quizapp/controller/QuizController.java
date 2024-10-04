f994c65199e971fee02a98fc978a697cfd70acf4
choudharymanisha <choudharymanisha38566@gmail.com>
2024-10-04 19:19:14 +0530

    Updated QuizController, QuizService, and added Response model

--- src/main/java/com/manisha/quizapp/controller/QuizController.java
+++ src/main/java/com/manisha/quizapp/controller/QuizController.java
@@ -1,7 +1,7 @@
 package com.manisha.quizapp.controller;
 
-import com.manisha.quizapp.model.Question;
 import com.manisha.quizapp.model.QuestionWrapper;
+import com.manisha.quizapp.model.Response;
 import com.manisha.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
+
     @Autowired
     QuizService quizService;
-    @PostMapping("create")
-    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
-return quizService.createQuiz(category,numQ,title);
-    }
-    @GetMapping("get/{id}")
-    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
-         return quizService.getQuizQuestions(id);
 
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
+        return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses) {
+        return quizService.calculatedResult(id, responses);
     }
 }
--- src/main/java/com/manisha/quizapp/service/QuizService.java
+++ src/main/java/com/manisha/quizapp/service/QuizService.java
@@ -5,6 +5,7 @@ import com.manisha.quizapp.dao.QuizDao;
 import com.manisha.quizapp.model.Question;
 import com.manisha.quizapp.model.QuestionWrapper;
 import com.manisha.quizapp.model.Quiz;
+import com.manisha.quizapp.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,30 +17,56 @@ import java.util.Optional;
 
 @Service
 public class QuizService {
+
     @Autowired
     QuizDao quizDao;
+
     @Autowired
     QuestionDao questionDao;
 
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
-        List<Question> questions = questionDao.findRandomQuestionByCategory(category,numQ);
+        List<Question> questions = questionDao.findRandomQuestionByCategory(category, numQ);
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
         quiz.setQuestions(questions);
         quizDao.save(quiz);
-        return new ResponseEntity<>("Success",HttpStatus.CREATED);
+        return new ResponseEntity<>("Quiz created successfully", HttpStatus.CREATED);
     }
 
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-        Optional<Quiz> quiz = quizDao.findById(id);
-        List<Question> questionsFromDB = quiz.get().getQuestions();
-        List<QuestionWrapper> questionsForUser = new ArrayList<>();
-        for(Question q : questionsFromDB){
-            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
-            questionsForUser.add(qw);
+        Optional<Quiz> quizOptional = quizDao.findById(id);
+        if (quizOptional.isPresent()) {
+            Quiz quiz = quizOptional.get();
+            List<Question> questionsFromDB = quiz.getQuestions();
+            List<QuestionWrapper> questionsForUser = new ArrayList<>();
+            for (Question q : questionsFromDB) {
+                QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+                questionsForUser.add(qw);
+            }
+            return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+        } else {
+            // Handle case where quiz with given ID does not exist
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
 
-        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
-
+    public ResponseEntity<Integer> calculatedResult(Integer id, List<Response> responses) {
+        Optional<Quiz> quizOptional = quizDao.findById(id);
+        if (quizOptional.isPresent()) {
+            Quiz quiz = quizOptional.get();
+            List<Question> questions = quiz.getQuestions();
+            int correctAnswers = 0;
+            int i = 0;
+            for (Response response : responses) {
+                if (response.getResponse().equals(questions.get(i).getRightAnswer())) {
+                    correctAnswers++;
+                }
+                i++;
+            }
+            return new ResponseEntity<>(correctAnswers, HttpStatus.OK);
+        } else {
+            // Handle case where quiz with given ID does not exist
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
